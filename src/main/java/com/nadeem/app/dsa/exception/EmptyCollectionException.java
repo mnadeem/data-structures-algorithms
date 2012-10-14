29219d9809673ddee672b931ad496b8ee626464f
Nadeem Mohammad <coolmind182006@gmail.com>
2012-10-14 22:22:22 +0530

    refactored

--- src/main/java/com/nadeem/app/dsa/exception/EmptyCollectionException.java
+++ src/main/java/com/nadeem/app/dsa/exception/EmptyCollectionException.java
@@ -3,9 +3,8 @@ package com.nadeem.app.dsa.exception;
 public class EmptyCollectionException extends RuntimeException {
 
 	private static final long serialVersionUID = 1L;
-	
+
 	public EmptyCollectionException() {
 		super("Collection Can't be empty");
 	}
-
 }
