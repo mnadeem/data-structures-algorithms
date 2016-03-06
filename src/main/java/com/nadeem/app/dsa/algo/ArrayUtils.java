9a0b3773416e52991c15788c0d82d7ca12817e85
Nadeem Mohammad <coolmind182006@gmail.com>
2016-03-06 11:00:07 +0530

    Algo: common elements in n sorted array

--- src/main/java/com/nadeem/app/dsa/algo/ArrayUtils.java
+++ src/main/java/com/nadeem/app/dsa/algo/ArrayUtils.java
@@ -891,4 +891,35 @@ public final class ArrayUtils {
 		}		
 		return result.toArray(new Integer[0]);
 	}
+
+	public static Integer[] commonElementsInNSortedArrays(int[][] arrays) {
+		int baseIndex = 0, currentIndex = 0, totalMatchFound= 0;
+		int[] indices = new int[arrays.length - 1];
+		boolean smallestArrayTraversed = false;
+		List<Integer> result = new ArrayList<Integer>();
+		while (!smallestArrayTraversed && baseIndex < arrays[0].length) {
+			totalMatchFound = 0;
+			for (int array = 1; array < arrays.length; array++) {
+				currentIndex = indices[array - 1];
+				while (currentIndex < arrays[array].length && arrays[array][currentIndex] < arrays[0][baseIndex]) {
+					currentIndex ++;					
+				}
+
+				if (currentIndex < arrays[array].length) {
+					if (arrays[array][currentIndex] == arrays[0][baseIndex]) {
+						totalMatchFound++;
+					}
+				} else {
+					smallestArrayTraversed = true;
+				}
+				indices[array - 1] = currentIndex;
+			}
+			if (totalMatchFound == arrays.length - 1) {
+				result.add(arrays[0][baseIndex]);
+			}
+			baseIndex++;
+		}
+
+		return result.toArray(new Integer[0]);
+	}
 }
--- src/test/java/com/nadeem/app/dsa/algo/ArrayUtilsTest.java
+++ src/test/java/com/nadeem/app/dsa/algo/ArrayUtilsTest.java
@@ -446,5 +446,27 @@ public class ArrayUtilsTest {
 		Integer result[] = ArrayUtils.commonElementsIn3SortedArrays(arr1, arr2, arr3);
 		assertThat(result, equalTo(new Integer[]{20, 80}));
 	}
+	
+	@Test
+	public void commonElementsInNSortedArrayTest() {
+		int arr[][] = { {1, 5, 10, 20, 40, 80},
+						{6, 7, 20, 80, 100},
+						{3, 4, 15, 20, 30, 70, 80, 120}
+					   };
+
+		Integer result[] = ArrayUtils.commonElementsInNSortedArrays(arr);
+		assertThat(result, equalTo(new Integer[]{20, 80}));
+		
+		arr = new int[][]{
+                {23, 34, 67, 89, 123, 566, 1000},
+                {11, 22, 23, 24,33, 37, 185, 566, 987, 1223, 1234},
+                {23, 43, 67, 98, 566, 678},
+                {1, 4, 5, 23, 34, 76, 87, 132, 566, 665},
+                {1, 2, 3, 23, 24, 344, 566}
+              };
+              
+        result = ArrayUtils.commonElementsInNSortedArrays(arr);
+      	assertThat(result, equalTo(new Integer[]{23, 566}));
+	}
 
 }
